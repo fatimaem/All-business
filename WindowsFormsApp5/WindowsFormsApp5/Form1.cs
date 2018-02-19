dcf1f48387cc2e97721909d61d92106c388604db
fatimaem <32015696+fatimaem@users.noreply.github.com>
2018-02-19 17:18:33 +0300

    Add files via upload

---
+++ WindowsFormsApp5/WindowsFormsApp5/Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public class survey
+        {
+            public string question;
+            public List<string> answers = new List<string>();
+            public GroupBox g;
+        }
+        int i = 50;
+        int v = 70;
+        //suallari ve cavablari saxlayacaq
+        List<survey> ga = new List<survey>();
+
+
+        StreamReader re = new StreamReader("C: /Users/Sony/Desktop/surveyy.txt");
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            while (!re.EndOfStream)
+            {
+                var yoxla = re.ReadLine();
+
+
+
+                if (yoxla.Length > 0)
+                {
+
+
+                    if (yoxla.Contains("Sual"))
+                    {
+
+                        survey opros = new survey();
+                        ga.Add(opros);
+                        opros.question = yoxla;
+                        for (var u = 0; u < 3;)
+                        {
+                            var yoxla2 = re.ReadLine();
+                            //cavablarin arasinda bow yer olarsa diye yoxlayiram
+                            if (yoxla2.Length > 0)
+                            {
+                                opros.answers.Add(yoxla2);
+                                u++;
+                            }
+
+
+                        }
+                    }
+
+                }
+
+            }
+       
+            foreach (var a in ga)
+            {
+                Label l = new Label();
+                a.g = new GroupBox();
+
+                a.g.Top = v;
+                v += 127;
+                this.Controls.Add(a.g);
+                l.Top = i;
+                i += 130;
+                l.Text = a.question;
+                l.Width = 400;
+                this.Controls.Add(l);
+                for (var w = 0; w < a.answers.Count; w++)
+                {
+                    int count = w * 20;
+                    RadioButton btn = new RadioButton();
+                    btn.Text = a.answers[w];
+
+                    btn.Top = btn.Height + count;
+
+
+
+                    a.g.Controls.Add(btn);
+                }
+            }
+            re.Close();
+
+        }
+        int result = 0;
+       
+        private void button2_Click(object sender, EventArgs e)
+        {
+            foreach (var o in ga)
+            {
+                RadioButton btn = (RadioButton)o.g.Controls[0];
+                if (btn.Checked)
+                {
+                    result += 1;
+                }
+            }
+            label1.Text = result.ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Form2 form = new Form2();
+            this.Hide();
+            form.Show();
+
+        }
+    
+    
+    }
+}
---
+++ WindowsFormsApp5/WindowsFormsApp5/Form2.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp5
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+
+        { 
+            StreamWriter write = new StreamWriter("C: /Users/Sony/Desktop/surveyy.txt",append:true);
+            write.WriteLine("Sual "+textBox1.Text);
+            write.WriteLine(textBox2.Text);
+            write.WriteLine(textBox3.Text);
+            write.WriteLine(textBox4.Text);
+            textBox1.Text = string.Empty;
+            textBox2.Text = string.Empty;
+            textBox3.Text = string.Empty;
+            textBox4.Text = string.Empty;
+            
+            MessageBox.Show("ur question has been successfully added");
+
+            write.Close();
+
+     
+        }
+    }
+}
